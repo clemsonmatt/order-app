e1dcf814a570e96ab7b6a3bea7a6f9d5271ede61
Matt Ellis <mellis@ncees.org>
2017-10-20 23:15:17 -0400

    Move orders to index. #5

--- index.php
+++ index.php
@@ -1,7 +1,39 @@
-<?php require_once('header.php'); ?>
+<?php
+
+require_once('header.php');
+
+require_once('entities/PurchaseOrderRepository.php');
+
+$repository = new PurchaseOrderRepository();
+$orders     = $repository->findAllOrders();
+
+?>
 
 <div class="container">
-    <h1>Orders</h1>
+    <table class="table table-hover">
+        <thead>
+            <th>ID</th>
+            <th>Order Account Name</th>
+            <th>Billing Account Number</th>
+            <th>Customer ID</th>
+            <th>Order ID</th>
+            <th>WIP MRC</th>
+            <th>Service Delivery Date</th>
+            <th>Pipeline Date</th>
+        </thead>
+        <?php foreach($orders as $order): ?>
+            <tr>
+                <td><?php echo $order->getId() ?></td>
+                <td><?php echo $order->getOrderAccountName() ?></td>
+                <td><?php echo $order->getBillingAccountNumber() ?></td>
+                <td><?php echo $order->getSrcCustomerId() ?></td>
+                <td><?php echo $order->getSrcOrderId() ?></td>
+                <td><?php echo $order->getWipMrc() ?></td>
+                <td><?php echo $order->getFirstSdAcceptedDate()->format('m/d/Y') ?></td>
+                <td><?php echo $order->getPipelineReportedDate()->format('m/d/Y') ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </div>
 
 <?php require_once('footer.php'); ?>
