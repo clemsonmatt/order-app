7f0c19175a9537521dd2d89346c63f9f48a297c8
Matt Ellis <mellis@ncees.org>
2017-10-20 21:22:03 -0400

    Basic login/logout functionality. #2

--- login.php
+++ login.php
@@ -7,7 +7,22 @@
 </head>
 <body style="background: #f4f5f6;">
     <div class="container">
-        <h1>Login</h1>
+        <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+            <h3 class="text-center" style="margin-top: 50px;">Login</h3>
+            <hr>
+            <?php if (isset($_GET['error']) == 1): ?>
+                <div class="alert alert-danger">Invalid credentials</div>
+            <?php endif; ?>
+
+            <form action="loginCheck.php" method="post">
+                <input type="text" name="username" class="form-control" placeholder="Username = demo">
+                <input type="password" name="password" class="form-control" placeholder="Password = pass">
+                <br>
+                <button type="submit" class="btn btn-primary btn-block">
+                    Login
+                </button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
---
+++ loginCheck.php
@@ -0,0 +1,16 @@
+<?php
+
+const REQUIRED_USERNAME = 'demo';
+const REQUIRED_PASSWORD = 'pass';
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if ($username == REQUIRED_USERNAME || $password == REQUIRED_PASSWORD) {
+    session_start();
+    $_SESSION['cso'] = md5($username);
+
+    header('location:index.php');
+} else {
+    header('location:login.php?error=1');
+}
