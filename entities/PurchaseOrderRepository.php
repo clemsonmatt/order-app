62631d6043808ba714bfefb69079d2fd9c549c38
Matt Ellis <mellis@ncees.org>
2017-10-24 00:34:46 -0400

    Basic search working. #7

--- entities/PurchaseOrderRepository.php
+++ entities/PurchaseOrderRepository.php
@@ -59,4 +59,42 @@ class PurchaseOrderRepository
 
         return [$purchaseOrder, $orderDetails];
     }
+
+    public function findBy(array $search)
+    {
+        $db   = new Database();
+        $conn = $db->getConnection();
+
+        $query = "
+            SELECT * FROM `EXAMPLE.TEST_DATA_SUMMARY` tds
+        ";
+
+        $counter = 0;
+
+        foreach ($search as $key => $value) {
+            $query .= ($counter == 0 ? " WHERE" : " AND")." ".$key." = ?";
+            $counter++;
+        }
+
+        $stmt = $conn->prepare($query);
+
+        foreach ($search as $key => $value) {
+            $stmt->bind_param($value['type'], $value['query']);
+        }
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $purchaseOrders = [];
+
+        while ($order = $result->fetch_assoc()) {
+            $purchaseOrder = new PurchaseOrder();
+            $purchaseOrder->hydrate($order);
+
+            $purchaseOrders[] = $purchaseOrder;
+        }
+
+        return $purchaseOrders;
+    }
 }
--- index.php
+++ index.php
@@ -1,15 +1,89 @@
 <?php
 
 require_once('header.php');
-
 require_once('entities/PurchaseOrderRepository.php');
+
+function getOrders(array $search) {
+    $purchaseRepository = new PurchaseOrderRepository();
+    $orders             = $purchaseRepository->findBy($search);
+
+    return $orders;
+}
+
+$possibleSearchParams = [
+    'PRIMARY_KEY'            => 'i',
+    'ORDER_ACCOUNT_NAME'     => 's',
+    'BILLING_ACCOUNT_NUMBER' => 'i',
+    'SRC_CUSTOMER_ID'        => 'i',
+    'SRC_ORDER_ID'           => 'i',
+    'WIP_MRC'                => 'i',
+    'FIRST_SD_ACCEPTED_DATE' => 's',
+    'PIPELINE_REPORTED_DATE' => 's',
+];
+
+$searchParams = [];
+
+foreach ($_POST as $key => $value) {
+    if (array_key_exists($key, $possibleSearchParams) && $value != '') {
+        $searchType = $possibleSearchParams[$key];
+
+        $searchParams[$key] = [
+            'type'  => $searchType,
+            'query' => $searchType == 'i' ? (int)$value : $value,
+        ];
+    }
+}
 
 $repository = new PurchaseOrderRepository();
 $orders     = $repository->findAllOrders();
 
+if (count($searchParams)) {
+    $orders = getOrders($searchParams);
+}
+
 ?>
 
 <div class="container">
+    <h4>
+        Search
+        <small>
+        <?php
+            foreach ($searchParams as $key => $value) {
+                echo '<span class="label label-default">'.$key.': "'.$value['query'].'"</span>';
+            }
+        ?>
+        </small>
+    </h4>
+    <form action="" method="post">
+        <div class="row">
+            <div class="col-md-3">
+                <input class="form-control" name="PRIMARY_KEY" placeholder="ID">
+                <br>
+                <input class="form-control" name="ORDER_ACCOUNT_NAME" placeholder="Order account name">
+            </div>
+            <div class="col-md-3">
+                <input class="form-control" name="BILLING_ACCOUNT_NUMBER" placeholder="Billing account number">
+                <br>
+                <input class="form-control" name="SRC_CUSTOMER_ID" placeholder="Customer ID">
+            </div>
+            <div class="col-md-3">
+                <input class="form-control" name="SRC_ORDER_ID" placeholder="Order ID">
+                <br>
+                <input class="form-control" name="WIP_MRC" placeholder="WIP MRC">
+            </div>
+            <div class="col-md-3">
+                <input class="form-control" name="FIRST_SD_ACCEPTED_DATE" placeholder="Service delivery date">
+                <br>
+                <input class="form-control" name="PIPELINE_REPORTED_DATE" placeholder="Pipeline date">
+            </div>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">
+            Search
+        </button>
+    </form>
+    <hr>
+    <h4>Orders</h4>
     <table class="table table-hover">
         <thead>
             <th>ID</th>
